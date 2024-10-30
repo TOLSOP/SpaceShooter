3e7006549593383cfb26f9ce4a6ccd6b974f8f96
TOLSOP <41379483+TOLSOP@users.noreply.github.com>
2024-10-30 10:21:45 +0200

    Clean up the code

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,48 +5,39 @@ using UnityEngine;
 using UnityEngine.UIElements;
 
 public class Player : MonoBehaviour
-{   
+{
     [SerializeField]
     private float _speed = 3.5f;
+
     [SerializeField]
     public GameObject _laserPrefab;
 
     [SerializeField]
     private float fireRate = 0.5f;
+
     [SerializeField]
     private float _canFire = -1f;
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        //take current position = new position (0,0,0)
-        transform.position = new Vector3(0, 0, 0);
-        
-    }
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         CalculateMovement();
 
-        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire)
+        if (Input.GetKey(KeyCode.Space) && Time.time > _canFire)
         {
-            _canFire = Time.time + fireRate;
-
-            Instantiate(_laserPrefab, transform.position + new Vector3(0, .78f, 0), Quaternion.identity);
-            
+            FireLaser();
         }
+
+
     }
 
-    void CalculateMovement()
-    { 
+    private void CalculateMovement()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
         Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
 
-        transform.Translate(direction* _speed * Time.deltaTime);
+        transform.Translate(direction * _speed * Time.deltaTime);
 
         if (transform.position.y >= 0)
         {
@@ -54,17 +45,23 @@ public class Player : MonoBehaviour
         }
         else if (transform.position.y <= -3.88f)
         {
-             transform.position = new Vector3(transform.position.x, -3.88f, 0);
+            transform.position = new Vector3(transform.position.x, -3.88f, 0);
         }
 
         if (transform.position.x >= 11.3f)
         {
             transform.position = new Vector3(-11.3f, transform.position.y, 0);
         }
-
         else if (transform.position.x < -11.3f)
         {
             transform.position = new Vector3(11.3f, transform.position.y, 0);
         }
+    }
+
+    private void FireLaser()
+    {
+        _canFire = Time.time + fireRate;
+
+        Instantiate(_laserPrefab, transform.position + new Vector3(0, .78f, 0), Quaternion.identity);
     }
 }
